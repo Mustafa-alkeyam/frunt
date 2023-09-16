eae2c4e53679f9feb90fd795d5df8e01fb8266df
Mustafa-alkeyam <falahmustafa751@gmail.com>
2023-09-16 19:42:42 +0300

    prime

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
 
 var myName = document.getElementsByClassName('myName')[0]
-myName.innerHTML = 'Ahmad'
+myName.innerHTML = 'Mustafa'
 myName.style.color = 'red'
+
+var primeForm = document.getElementsByClassName('primeForm')[0]
+var primeInput = document.getElementById('primeInput')
+var primeResult = document.getElementsByClassName('primeResult')[0]
+
+primeForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(e)
+    var primeOrNot=parseInt(primeInput.value);
+    for (var i =2; i <primeOrNot; i++){
+        if (primeOrNot % i == 0){
+            primeResult.innerHTML = "not prime"
+            break
+        }
+        else{
+            primeResult.innerHTML = "prime"
+        }
+    }
+})
+
+
+// example of do while:
+var num = 5;
+do{
+    console.log(num);
+    num--;
+}
+while(num>=0)
