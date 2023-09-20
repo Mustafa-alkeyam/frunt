c649ff2bb8d93d8daacb3a9bdf4197e21f658702
Mustafa-alkeyam <falahmustafa751@gmail.com>
2023-09-20 13:39:10 +0300

    addt

--- js/append.js
+++ js/append.js
@@ -1,7 +1,10 @@
 console.log(document.body.children)
-let h1Js = document.createElement("h1");
-h1Js.innerHTML = "i am h1 from js";
-document.body.append(h1Js);
-let newButton = document.createElement("button");
-newButton.innerHTML = "clek";
-document.body.append(newButton)
+// let tabel1 = document.createElement("table");
+// tabel1.innerHTML ;
+// document.body.append(tabel1);
+// let tbody5 = document.createElement("tbody");
+// tbody5.innerHTML;
+// document.body.table.append(tbody5);
+let trow3 = document.createElement("tr");
+trow3.innerHTML;
+document.tbody.append(trow3);
